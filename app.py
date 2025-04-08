5817b6c734f9cd78ee5ccae0c83ec34d8ec2d078
nicomar0807 <jonati883@gmail.com>
2025-04-08 12:22:06 -0500

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 # Tokens y configuración
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "mi_token_secreto")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "EAAItRKRWhG4BO1QZAhnRz7ecNnNsJhniLZAb6iMlPy2M1MQ0QwFTzEVOrtmo39fOlGZAaLUmoSf7N3UJZCDPa3m95ni9O2xGJASH9uY99M53bnElELB890QWlY0QOyewBvENqb91ZCDLTxIanuN5ePHUjLS8OXbyukJIBhLWWjZAIMwgZCANwzZBaUGE")
-PAGE_ID = os.getenv("608583202336837")
+PAGE_ID = os.getenv("PAGE_ID")
 
 # Inicializar Flask
 app = Flask(__name__)
