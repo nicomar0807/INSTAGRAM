682c1ee3d06c18566b161a3b91c659e113fdf74e
nicomar0807 <jonati883@gmail.com>
2025-04-10 11:28:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -71,9 +71,9 @@ def send_message(recipient_id, message_text):
     }
     headers = {"Content-Type": "application/json"}
     INSTAGRAM_ID = os.getenv("INSTAGRAM_ID")  # Nueva variable en Render
-url = f"https://graph.facebook.com/v17.0/{INSTAGRAM_ID}/messages?access_token={ACCESS_TOKEN}"
+    url = f"https://graph.facebook.com/v17.0/{INSTAGRAM_ID}/messages?access_token={ACCESS_TOKEN}"
 
-     response = requests.post(url, headers=headers, json=payload)
+    response = requests.post(url, headers=headers, json=payload)
     print(f"📤 Enviando mensaje a {recipient_id}: {message_text}")
     print(f"🔁 Respuesta send_message: {response.status_code} - {response.text}")
 
